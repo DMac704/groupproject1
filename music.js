989015acbc95aa16810b4ee29a96054dc3f257e9
jboe26 <joshrboepple@outlook.com>
2019-08-27 17:26:07 -0400

    take a look guys, added some comments on what we need

---
+++ music.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+
+
+
+// incorporate ajax and api keys for youtube and wikipedia
+
+
+
+
+
+
+
+
+
+
+// add a search button for when you click it goes to the screen with the youtube player and the search results for a artist
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
